3cd2a57a2054fedb21b72a236f21e15f3c6a4208
pawind <paul.teamwindheuser@gmail.com>
2021-05-15 22:08:25 +0200

    Implementation Creating/Deleting bars
    ars

--- src/App.js
+++ src/App.js
@@ -5,6 +5,37 @@ import SortingDisplay from "./components/SortingDisplay/SortingDisplay";
 
 const App = () => {
   const [data, setData] = useState([7, 8, 5, 1, 352, 75, 4, 2, 5, 35]);
+  const [barAmount, setBarAmount] = useState(10);
+
+  const createData = (amount) => {
+    if(amount === barAmount){
+      return;
+    }
+    else if (amount > barAmount) {                          //Some more Bars need to created
+      for (let i = barAmount; i < amount; i++) {
+       // console.log(i)
+        setData((prevData) => {
+          //console.log(prevData)
+          let d = [...prevData];
+          let random = Math.round(Math.random()*800);
+          d.push(random);
+          return d
+        })
+        setBarAmount(amount);
+      }
+    }else{
+      console.log("gfieagfoijeaf")
+      for(let i=amount; i<barAmount; i++){
+        console.log(i)
+        setData(prevData => {
+          let d = [...prevData]
+          d.pop()
+          return d;
+        })
+      }
+      setBarAmount(amount);
+    }
+  }
 
   return (
     <div className="App" style={{}}>
--- src/components/SortingDisplay/SortingDisplay.js
+++ src/components/SortingDisplay/SortingDisplay.js
@@ -5,14 +5,13 @@ import classes from "./SortingDisplay.module.css";
 const sortingDisplay = (props) => {
   const displayHeight = 900;
   let displayHeightCss = displayHeight + "" + "px";
-
   return (
     <div
       className={classes.sortingDisplay}
       style={{ height: displayHeightCss }}
     >
-      {props.data.map((barHeight) => {
-        return <Bar barHeight={barHeight} displayHeight={displayHeight} />;
+      {props.data.map((barHeight, i) => {
+        return <Bar key={i} barHeight={barHeight} displayHeight={displayHeight} />;
       })}
     </div>
   );
