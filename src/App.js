a1bd1dd5ee6bf897cd464f5b0cf0acf14469abdf
pawin <paul.teamwindheuser@gmail.com>
2021-05-16 10:12:02 +0200

    Creation of bars by slider and instant bbutton

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ControlBar from "./components/ControlBar/ControlBar";
 import SortingDisplay from "./components/SortingDisplay/SortingDisplay";
 
 const App = () => {
-  const [data, setData] = useState([7, 8, 5, 1, 352, 75, 4, 2, 5, 35]);
-  const [barAmount, setBarAmount] = useState(10);
+  const [data, setData] = useState([]);
+  const [barAmount, setBarAmount] = useState(0);
+  const [newBarAmount, setNewBarAmount] = useState(0);
 
-  const createData = (amount) => {
-    if(amount === barAmount){
+  const createData = () => {
+    setData([]);
+    if (newBarAmount === barAmount) {
       return;
-    }
-    else if (amount > barAmount) {                          //Some more Bars need to created
-      for (let i = barAmount; i < amount; i++) {
-       // console.log(i)
+    } else if (newBarAmount > barAmount) {
+      //Some more Bars need to created
+      for (let i = barAmount; i < newBarAmount; i++) {
         setData((prevData) => {
-          //console.log(prevData)
           let d = [...prevData];
-          let random = Math.round(Math.random()*800);
+          let random = Math.round(Math.random() * 800);
           d.push(random);
-          return d
-        })
-        setBarAmount(amount);
-      }
-    }else{
-      console.log("gfieagfoijeaf")
-      for(let i=amount; i<barAmount; i++){
-        console.log(i)
-        setData(prevData => {
-          let d = [...prevData]
-          d.pop()
           return d;
-        })
+        });
+        setBarAmount(newBarAmount);
       }
-      setBarAmount(amount);
+    } else {
+      for (let i = newBarAmount; i < barAmount; i++) {
+        setData((prevData) => {
+          let d = [...prevData];
+          d.pop();
+          return d;
+        });
+      }
+      setBarAmount(newBarAmount);
     }
-  }
+    console.log(data);
+  };
+
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const createBars = () => {
+    setBarAmount(0);
+    setNewBarAmount(randomIntFromInterval(90, 108));
+  };
+
+  useEffect(() => {
+    createData();
+  }, [newBarAmount]);
 
   return (
     <div className="App" style={{}}>
-      <ControlBar />
+      <ControlBar
+        amount={newBarAmount}
+        handleAmountChange={setNewBarAmount}
+        createBars={createBars}
+      />
       <SortingDisplay data={data} />
     </div>
   );
--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -1,5 +1,19 @@
 const controlBar = (props) => {
-  return <div>ControlBar</div>;
+  return (
+    <div>
+      {/* <input
+        type="range"
+        step="10"
+        min="0"
+        max="120"
+        value={props.amount}
+        onInput={(e) => props.handleAmountChange(e.target.value)}
+      ></input> */}
+      {/* Slider Solution */}
+
+      <input type="button" value="Create" onClick={props.createBars} />
+    </div>
+  );
 };
 
 export default controlBar;
