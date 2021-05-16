ad2bddd6fcc23433af5d1f109dfb8dc1bfa649cb
pawin <paul.teamwindheuser@gmail.com>
2021-05-16 09:12:33 +0200

    Removed unnecesary Stringaddition

--- src/components/SortingDisplay/SortingDisplay.js
+++ src/components/SortingDisplay/SortingDisplay.js
@@ -4,14 +4,16 @@ import classes from "./SortingDisplay.module.css";
 
 const sortingDisplay = (props) => {
   const displayHeight = 900;
-  let displayHeightCss = displayHeight + "" + "px";
+  let displayHeightCss = displayHeight + "px";
   return (
     <div
       className={classes.sortingDisplay}
       style={{ height: displayHeightCss }}
     >
       {props.data.map((barHeight, i) => {
-        return <Bar key={i} barHeight={barHeight} displayHeight={displayHeight} />;
+        return (
+          <Bar key={i} barHeight={barHeight} displayHeight={displayHeight} />
+        );
       })}
     </div>
   );
